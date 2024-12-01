fffd15d7a1d0cd11084b75f755572579d1c7eedd
Sukrit <debsukrit00@gmail.com>
2024-12-01 21:27:04 +0530

    Plotted multiple plots on one graph

---
+++ Matplot/Saving.py
@@ -0,0 +1,11 @@
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.arange(-3, 3, 0.01)
+
+plt.plot(x, norm.pdf(x))
+plt.plot(x, norm.pdf(x, 1.0, 0.5))
+plt.show()
+
+plt.savefig('MyPlot.png', format='png')
