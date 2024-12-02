f69bc635ea8b031dd4f7835a2834ad8b8e71e10e
Sukrit <debsukrit00@gmail.com>
2024-12-02 23:17:02 +0530

    Bar chart

---
+++ Matplot/Bar-chart.py
@@ -0,0 +1,8 @@
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+import numpy as np
+
+values = [120, 55, 4, 32, 140]
+colors = ['r', 'g', 'b', 'c', 'm']
+plt.bar(range(0,5), values, color= colors)
+plt.show()
