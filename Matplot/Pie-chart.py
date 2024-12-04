8256afa8b6d58c853d388503928b86b1e9f9e70d
Sukrit Deb <152785863+SukritDeb@users.noreply.github.com>
2024-12-04 22:45:46 +0530

    Update Pie-chart.py

--- Matplot/Pie-chart.py
+++ Matplot/Pie-chart.py
@@ -8,6 +8,6 @@ values = [12, 55, 4, 22, 104]
 colors = ['r', 'g', 'b', 'c', 'm']
 explode = [0, 0, 0.2, 0, 0]
 labels = ['India', 'United States', 'Russia', 'China', 'UK']
-plt.pie(values, colors= colors, labels=labels, explode = explode)
+plt.pie(values, colors= colors, labels=labels, explode = explode) #plots a Pie chart
 plt.title('Student Locations')
 plt.show()
