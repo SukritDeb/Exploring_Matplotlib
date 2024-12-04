c68c810548f1263bc32ed32cc029799fbea44f80
Sukrit Deb <152785863+SukritDeb@users.noreply.github.com>
2024-12-04 22:43:13 +0530

    Update Box-&-Whisker-Plot.py

--- Matplot/Box-&-Whisker-Plot.py
+++ Matplot/Box-&-Whisker-Plot.py
@@ -6,5 +6,5 @@ uniformSkewed = np.random.rand(100) * 100 - 40
 high_outliers = np.random.rand(10) * 50 + 100
 low_outliers = np.random.rand(10) * -50 - 100
 data = np.concatenate((uniformSkewed, high_outliers, low_outliers))
-plt.boxplot(data)
+plt.boxplot(data) #plots Box & whisker graph
 plt.show()
