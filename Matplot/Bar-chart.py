719475e608d7778481967ce62195f438c424c376
Sukrit Deb <152785863+SukritDeb@users.noreply.github.com>
2024-12-04 22:40:39 +0530

    Update Bar-chart.py

--- Matplot/Bar-chart.py
+++ Matplot/Bar-chart.py
@@ -4,5 +4,5 @@ import numpy as np
 
 values = [120, 55, 4, 32, 140]
 colors = ['r', 'g', 'b', 'c', 'm']
-plt.bar(range(0,5), values, color= colors)
+plt.bar(range(0,5), values, color= colors) #plots bar chart
 plt.show()
