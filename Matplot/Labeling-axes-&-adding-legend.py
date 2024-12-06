1069c88b3e37710d5fa4f8038707d724cd3fed31
Sukrit Deb <152785863+SukritDeb@users.noreply.github.com>
2024-12-06 23:22:12 +0530

    Update Labeling-axes-&-adding-legend.py

--- Matplot/Labeling-axes-&-adding-legend.py
+++ Matplot/Labeling-axes-&-adding-legend.py
@@ -11,7 +11,7 @@ axes.set_xticks([-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5])
 axes.set_yticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
 axes.grid()
 plt.xlabel('Greebles') #Labeling x axis
-plt.ylabel('Probability')
+plt.ylabel('Probability') #Labeling y axis
 plt.plot(x, norm.pdf(x), 'b-')
 plt.plot(x, norm.pdf(x, 1.0, 0.5), 'r:')
 plt.legend(['Sneetches', 'Gacks'], loc=4)
