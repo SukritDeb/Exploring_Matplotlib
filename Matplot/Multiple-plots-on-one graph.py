3d809b581bff4663191048229a04df0be329dc44
Sukrit Deb <152785863+SukritDeb@users.noreply.github.com>
2024-12-06 23:23:57 +0530

    Update Multiple-plots-on-one graph.py

--- Matplot/Multiple-plots-on-one graph.py
+++ Matplot/Multiple-plots-on-one graph.py
@@ -5,5 +5,5 @@ import numpy as np
 x = np.arange(-3, 3, 0.01)
 
 plt.plot(x, norm.pdf(x)) #first line plotting
-plt.plot(x, norm.pdf(x, 1.0, 0.5))
+plt.plot(x, norm.pdf(x, 1.0, 0.5)) #second line plotting
 plt.show()
